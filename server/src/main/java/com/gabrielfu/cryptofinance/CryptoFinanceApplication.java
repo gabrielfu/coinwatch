eee882ab54a8fe5c633f5d59a0abc6effc415d1b
gabrielfu <hfu.gabriel@gmail.com>
2023-03-20 15:51:30 +0800

    spring initializr

---
+++ server/src/main/java/com/gabrielfu/cryptofinance/CryptoFinanceApplication.java
@@ -0,0 +1,13 @@
+package com.gabrielfu.cryptofinance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoFinanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptofinanceApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/gabrielfu/cryptofinance/CryptoFinanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabrielfu.cryptofinance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoFinanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
