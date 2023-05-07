eb48693940042d1c6d794c3fbf8d6d82785bf08e
Gabriel Fu <hfu.gabriel@gmail.com>
2023-05-07 23:04:51 +0800

    add @RequestParam

--- server/src/main/java/com/gabrielfu/cryptoportfoliotracker/quote/QuoteController.java
+++ server/src/main/java/com/gabrielfu/cryptoportfoliotracker/quote/QuoteController.java
@@ -3,6 +3,7 @@ package com.gabrielfu.cryptoportfoliotracker.quote;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,12 +13,12 @@ public class QuoteController {
     private final QuoteService quoteService;
 
     @GetMapping("spot")
-    public SpotPriceDTO getTokenSpotPrice(String token) {
+    public SpotPriceDTO getTokenSpotPrice(@RequestParam String token) {
         return quoteService.getTokenSpotPrice(token);
     }
 
     @GetMapping("historical")
-    public HistoricalPricesDTO getTokenHistoricalPrices(String token) {
+    public HistoricalPricesDTO getTokenHistoricalPrices(@RequestParam String token) {
         return quoteService.getTokenHistoricalPrices(token);
     }
 }
