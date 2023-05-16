f9c3390c89877c0dc9c7ad2c8e82d584222ffb2b
Gabriel Fu <hfu.gabriel@gmail.com>
2023-05-16 22:04:06 +0800

    fetch real token data

--- frontend/app/components/token/TokenOverview.tsx
+++ frontend/app/components/token/TokenOverview.tsx
@@ -1,16 +1,64 @@
 'use client';
 
-import { useRouter } from "next/navigation";
 import { Text } from "rebass";
 import Card from "../../components/Card";
 import TokenTable from "./TokenTable";
 import { AutoColumn } from "../Column";
-import useAllTokenData from "@/dummy-data/all-tokens";
+import { useEffect, useState } from "react";
+
+interface TokenData {
+  name: string;
+  symbol: string;
+  price: number;
+  priceChange: number;
+  volume: number;
+  marketCap: number;
+  logo: string;
+}
+
+interface TokenInfo {
+  name: string;
+  symbol: string;
+  image: string;
+}
+
+interface QuoteData {
+  symbol: string;
+  price: number;
+  priceChange: number;
+  priceChangePercent: number;
+}
 
 const TokenOverview = () => {
-  const router = useRouter();
+  const [data, setData] = useState<TokenData[]>([]);
+  const [isLoading, setLoading] = useState(false);
 
-  const data = useAllTokenData();
+  useEffect(() => {
+    setLoading(true);
+    fetch("http://localhost:8080/api/v1/tokens")
+      .then((res) => res.json())
+      .then((tokenInfos: TokenInfo[]) => {
+        const symbols = tokenInfos.map(t => t.symbol).join(",");
+        fetch(`http://localhost:8080/api/v1/quote/batch-spot?tokens=${symbols}`)
+          .then((res) => res.json())
+          .then((quoteData: QuoteData[]) => {
+            const tokenDatas: TokenData[] = tokenInfos.map((t, i) => {
+              const q = quoteData[i];
+              return {
+                name: t.name,
+                symbol: t.symbol,
+                price: q.price,
+                priceChange: q.priceChangePercent,
+                volume: 0,
+                marketCap: 0,
+                logo: t.image,
+              }
+            })
+            setData(tokenDatas);
+            setLoading(false);
+          })
+      });
+  }, []);
 
   return ( 
     <Card>
