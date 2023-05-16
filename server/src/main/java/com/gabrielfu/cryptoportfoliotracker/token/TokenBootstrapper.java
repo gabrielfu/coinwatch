b2d7c21e11bd2877a4c9f622a7706c60fcf1d7fd
Gabriel Fu <hfu.gabriel@gmail.com>
2023-05-16 18:37:55 +0800

    use EventListener for bootstrapping instead

--- server/src/main/java/com/gabrielfu/cryptoportfoliotracker/token/TokenBootstrapper.java
+++ server/src/main/java/com/gabrielfu/cryptoportfoliotracker/token/TokenBootstrapper.java
@@ -3,15 +3,15 @@ package com.gabrielfu.cryptoportfoliotracker.token;
 import com.gabrielfu.cryptoportfoliotracker.token.bootstrap.CoinGeckoClient;
 import com.gabrielfu.cryptoportfoliotracker.token.bootstrap.CoinGeckoResponseMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.ApplicationArguments;
-import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Component
-public class TokenBootstrapper implements ApplicationRunner {
+public class TokenBootstrapper {
     @Autowired
     private CoinGeckoClient coinGeckoClient;
 
@@ -24,7 +24,8 @@ public class TokenBootstrapper implements ApplicationRunner {
                 .collect(Collectors.toList());
     }
 
-    public void run(ApplicationArguments args) {
+    @EventListener(ApplicationReadyEvent.class)
+    public void bootstrap() {
         List<Token> tokens = getTokens();
         tokens.forEach(tokenService::createToken);
     }
