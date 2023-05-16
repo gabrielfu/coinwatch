ce2183a6acc5390d711c7dd13f165ab7b58971e2
Gabriel Fu <hfu.gabriel@gmail.com>
2023-05-16 14:26:56 +0800

    match parent href

--- frontend/app/components/navbar/NavLink.tsx
+++ frontend/app/components/navbar/NavLink.tsx
@@ -4,7 +4,7 @@ import Link, { LinkProps } from "next/link";
 
 const NavLink = (props: React.PropsWithChildren<LinkProps>) => {
   const pathname = usePathname();
-  const isActive = pathname === props.href || pathname === props.as;
+  const isActive = pathname.startsWith(props.href.toString());
   const className = "text-gmx-text text-lg font-medium px-4 py-2 " 
     + (isActive ? "rounded-2xl bg-gmx-light text-white" : "hover:text-white hover:underline hover:underline-offset-8");
 
