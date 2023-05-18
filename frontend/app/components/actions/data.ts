e3ef85520fb85223031ae226e1088c6021509b22
Gabriel Fu <hfu.gabriel@gmail.com>
2023-05-18 13:14:18 +0800

    refactor: extracted get token datas

---
+++ frontend/app/components/actions/data.ts
@@ -0,0 +1,56 @@
+
+
+interface TokenData {
+  name: string;
+  symbol: string;
+  price: number;
+  priceChange: number;
+  volume: number;
+  marketCap: number;
+  logo: string;
+}
+
+interface TokenInfo {
+  name: string;
+  symbol: string;
+  logo: string;
+}
+
+interface QuoteData {
+  symbol: string;
+  price: number;
+  priceChange: number;
+  priceChangePercent: number;
+  volume: number;
+  marketCap: number;
+}
+
+export const getTokenWithQuoteDatas = async () => {
+  return await fetch("http://localhost:8080/api/v1/tokens")
+    .then((res) => res.json())
+    .then(async (tokenInfos: TokenInfo[]) => {
+      const symbols = tokenInfos.map(t => t.symbol).join(",");
+      const innerRes = await fetch(`http://localhost:8080/api/v1/quote/batch-spot?tokens=${symbols}`)
+        .then((res) => res.json())
+        .then((quoteDatas: QuoteData[]) => {
+          const tokenDatas: TokenData[] = tokenInfos
+            .map((t, i) => {
+              const q = quoteDatas[i];
+              return q == null
+                ? null
+                : {
+                  name: t.name,
+                  symbol: t.symbol,
+                  price: q.price,
+                  priceChange: q.priceChangePercent,
+                  volume: q.volume,
+                  marketCap: q.marketCap,
+                  logo: t.logo,
+                }
+            })
+            .filter(t => (t != null) && (t.marketCap > 1_000_000));
+          return tokenDatas;
+        });
+        return innerRes;
+    });
+}
--- frontend/app/components/token/TokenOverview.tsx
+++ frontend/app/components/token/TokenOverview.tsx
@@ -5,31 +5,7 @@ import TokenTable from "./TokenTable";
 import { AutoColumn } from "../Column";
 import { useEffect, useState } from "react";
 import { Label } from "../Text";
-
-interface TokenData {
-  name: string;
-  symbol: string;
-  price: number;
-  priceChange: number;
-  volume: number;
-  marketCap: number;
-  logo: string;
-}
-
-interface TokenInfo {
-  name: string;
-  symbol: string;
-  logo: string;
-}
-
-interface QuoteData {
-  symbol: string;
-  price: number;
-  priceChange: number;
-  priceChangePercent: number;
-  volume: number;
-  marketCap: number;
-}
+import { getTokenWithQuoteDatas } from "../actions/data";
 
 const TokenOverview = () => {
   const [data, setData] = useState<TokenData[]>([]);
@@ -37,33 +13,11 @@ const TokenOverview = () => {
 
   useEffect(() => {
     setLoading(true);
-    fetch("http://localhost:8080/api/v1/tokens")
-      .then((res) => res.json())
-      .then((tokenInfos: TokenInfo[]) => {
-        const symbols = tokenInfos.map(t => t.symbol).join(",");
-        fetch(`http://localhost:8080/api/v1/quote/batch-spot?tokens=${symbols}`)
-          .then((res) => res.json())
-          .then((quoteDatas: QuoteData[]) => {
-            const tokenDatas: TokenData[] = tokenInfos
-              .map((t, i) => {
-                const q = quoteDatas[i];
-                return q == null
-                  ? null
-                  : {
-                    name: t.name,
-                    symbol: t.symbol,
-                    price: q.price,
-                    priceChange: q.priceChangePercent,
-                    volume: q.volume,
-                    marketCap: q.marketCap,
-                    logo: t.logo,
-                  }
-              })
-              .filter(t => (t != null) && (t.marketCap > 1_000_000));
-            setData(tokenDatas);
-            setLoading(false);
-          })
-      });
+    getTokenWithQuoteDatas()
+      .then((tokenDatas) => {
+        setData(tokenDatas);
+        setLoading(false);
+      })
   }, []);
 
   return ( 
