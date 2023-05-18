2840686043c37e2c14eaa10da9fd2cecb90c73ae
Gabriel Fu <hfu.gabriel@gmail.com>
2023-05-18 16:01:21 +0800

    layout

--- frontend/app/components/charts/RangeSelector.tsx
+++ frontend/app/components/charts/RangeSelector.tsx
@@ -1,6 +1,27 @@
+import { twColors } from "@/app/twConfig";
+import Card from "../Card";
+import { Label } from "../Text";
+
 const RangeSelector = () => {
   return ( 
-    <div className="text-white">Range selector</div>
+    <Label backgroundColor="transparent" color={twColors.gmx.text}
+      type="text"
+      justifyContent="space-between"
+      style={{ 
+        borderRadius: "8px", 
+        borderWidth: "2px", 
+        borderColor: twColors.gmx.text
+      }}>
+      <Label className="border-r-2 border-gmx-text py-1 px-3">
+        1D
+      </Label>
+      <Label className="border-r-2 border-gmx-text py-1 px-3">
+        5D
+      </Label>
+      <Label className="py-1 px-3">
+        1M
+      </Label>
+    </Label>
    );
 }
  
