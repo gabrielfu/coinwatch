5842b772eff1762a0912e024871b1426285547e1
Gabriel Fu <hfu.gabriel@gmail.com>
2023-05-29 12:00:50 +0800

    remove styled components from row

--- frontend/app/components/Row.tsx
+++ frontend/app/components/Row.tsx
@@ -1,70 +1,27 @@
-import styled from 'styled-components'
-import { Box, BoxProps } from 'rebass/styled-components'
-import React from 'react';
+import { Box, BoxProps } from 'rebass'
 
-interface RowProps extends BoxProps {
-  children: React.ReactNode;
-  width?: string;
-  align?: string;
-  justify?: string;
-  padding?: string;
-  border?: string;
-  borderRadius?: string;
+const Row = (props: BoxProps) => {
+  return ( 
+    <Box
+      className="flex"
+      width={props.width ?? "100%"}
+      alignItems={props.alignItems ?? "center"}
+      justifyContent={props.justifyContent ?? "flex-start"}
+      {...props}
+    />
+   );
 }
 
-// FIXME
-const Row = ({
-  width = 'auto',
-  align = 'center',
-  justify = 'start',
-  padding,
-  border,
-  borderRadius,
-  children
-}: RowProps) => {
-  const classNames = `flex w-${width} justify-${justify} items-${align} ${padding ? `p-${padding}` : ''} ${border ? `border-${border}` : ''} ${borderRadius ? `rounded-${borderRadius}` : ''}`;
-
+export const RowBetween = (props: BoxProps) => {
   return (
-    <Box className={classNames}>
-      {children}
-    </Box>
-  );
-};
-
-export const RowBetween = (props: RowProps) => {
-  return (
-    <Row justify="between" {...props} />
+    <Row justifyContent="space-between" {...props} />
   )
 }
 
-// export const RowFlat = styled.div`
-//   display: flex;
-//   align-items: flex-end;
-// `
-
-// export const AutoRow = styled(Row)<{ gap?: string; justify?: string }>`
-//   flex-wrap: wrap;
-//   margin: ${({ gap }) => gap && `-${gap}`};
-//   justify-content: ${({ justify }) => justify && justify};
-
-//   & > * {
-//     margin: ${({ gap }) => gap} !important;
-//   }
-// `
-
-export const RowFixed = ({ gap, children }: { gap?: string; children: React.ReactNode }) => {
+export const RowFixed = (props: BoxProps) => {
   return (
-    <Row margin={gap || 0} width="fit">
-      {children}
-    </Row>
+    <Row margin={0} width="fit" {...props} />
   )
 }
-
-export const ResponsiveRow = styled(RowBetween)`
-  ${({ theme }) => theme.mediaWidth.upToSmall`
-    flex-direction: column;
-    row-gap: 1rem;
-  `};
-`
 
 export default Row;
