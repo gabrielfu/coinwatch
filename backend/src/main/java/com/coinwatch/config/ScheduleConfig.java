30f7787755047c0ac58e4dff37358c5511dab699
Gabriel Fu <hfu.gabriel@gmail.com>
2023-05-30 13:33:01 +0800

    cache getting quotes

---
+++ backend/src/main/java/com/coinwatch/config/ScheduleConfig.java
@@ -0,0 +1,9 @@
+package com.coinwatch.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
+public class ScheduleConfig {
+}
--- backend/src/main/java/com/coinwatch/quote/AggregateQuoteServiceImpl.java
+++ backend/src/main/java/com/coinwatch/quote/AggregateQuoteServiceImpl.java
@@ -2,6 +2,9 @@ package com.coinwatch.quote;
 
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -14,22 +17,29 @@ public class AggregateQuoteServiceImpl implements QuoteService {
     private final CoinCapQuoteServiceImpl coinCapQuoteService;
 
     @Override
+    @Cacheable("quoteSpot")
     public SpotPriceDTO getTokenSpotPrice(String token) {
         return coinCapQuoteService.getTokenSpotPrice(token);
     }
 
     @Override
+    @Cacheable("quoteBatchSpot")
     public List<SpotPriceDTO> batchGetTokenSpotPrice(List<String> tokens) {
         return coinCapQuoteService.batchGetTokenSpotPrice(tokens);
     }
 
     @Override
     public HistoricalPricesDTO getTokenHistoricalPrices(String token) {
-        return yahooQuoteService.getTokenHistoricalPrices(token);
+        return getTokenHistoricalPrices(token, null, null);
     }
 
     @Override
+    @Cacheable("quoteHistorical")
     public HistoricalPricesDTO getTokenHistoricalPrices(String token, String interval, String range) {
         return yahooQuoteService.getTokenHistoricalPrices(token, interval, range);
     }
+
+    @Scheduled(fixedRate = 60000L)
+    @CacheEvict(cacheNames = {"quoteSpot", "quoteBatchSpot", "quoteHistorical"}, allEntries = true)
+    public void clearCache() {}
 }
