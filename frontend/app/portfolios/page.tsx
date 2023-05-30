da3165dd5b6be2e182d5a8c836c9f9f30ea46de6
Gabriel Fu <hfu.gabriel@gmail.com>
2023-05-30 16:52:19 +0800

    fix click area

--- frontend/app/portfolios/page.tsx
+++ frontend/app/portfolios/page.tsx
@@ -22,23 +22,25 @@ const PortfoliosPage = () => {
   return ( 
     <Card>
       <AutoColumn gap="8px">
-      <div className="flex w-full justify-between">
+        <div className="flex w-full justify-between">
           <Label ml="16px" color="white" fontSize={24}>Portfolios</Label>
-          <Label backgroundColor={twColors.gmx.gray}
-            marginBottom="12px" 
-            padding="6px 12px" 
-            width={180}
-            height={42}
-            justifyContent="space-between"
-            style={{ 
-              borderRadius: "8px",
-            }}
-          >
-            <IoAddCircleOutline color="white" size={22} />
-            <Box className="text-white text-[16px] font-semilight hover:cursor-pointer" margin="0 6px">
-              Add Portfolio
-            </Box>
-          </Label>
+          <div className="hover:cursor-pointer">
+            <Label backgroundColor={twColors.gmx.gray}
+              marginBottom="12px" 
+              padding="6px 12px" 
+              width={180}
+              height={42}
+              justifyContent="space-between"
+              style={{ 
+                borderRadius: "8px",
+              }}
+            >
+              <IoAddCircleOutline color="white" size={22} />
+              <Box className="text-white text-[16px] font-semilight" margin="0 6px">
+                Add Portfolio
+              </Box>
+            </Label>
+          </div>
         </div>
         <PortfolioTable />
       </AutoColumn>
