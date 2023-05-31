16535cddd17337c6263588f37da6d9f430c91fca
Gabriel Fu <hfu.gabriel@gmail.com>
2023-05-31 13:28:14 +0800

    fix types

--- frontend/app/twConfig.ts
+++ frontend/app/twConfig.ts
@@ -1,6 +1,5 @@
 import resolveConfig from 'tailwindcss/resolveConfig';
 import tailwindConfig from '@/tailwind.config.js';
-import { RecursiveKeyValuePair } from 'tailwindcss/types/config';
 const twConfig = resolveConfig(tailwindConfig);
-export const twColors = twConfig.theme.colors as RecursiveKeyValuePair<string, string>;
+export const twColors = twConfig.theme?.colors as { [key: string]: string };
 export default twConfig;
