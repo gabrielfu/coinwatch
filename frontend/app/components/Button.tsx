62467c2c9534f3f84af570463ea8fec9dbc51b46
Gabriel Fu <hfu.gabriel@gmail.com>
2023-05-31 16:12:20 +0800

    polishing

--- frontend/app/components/Button.tsx
+++ frontend/app/components/Button.tsx
@@ -38,16 +38,7 @@ const Button = ({
         ${small ? 'border-[1px]' : 'border-2'}
       `}
     >
-      {Icon && (
-        <Icon
-          size={24}
-          className="
-            absolute
-            left-4
-            top-3
-          "
-        />
-      )}
+      {Icon && <Icon size={24} className="absolute left-4 top-3"/>}
       {label}
     </button>
    );
--- frontend/app/components/modals/Modal.tsx
+++ frontend/app/components/modals/Modal.tsx
@@ -53,15 +53,7 @@ export const Input = ({
   return (
     <div className="w-full relative">
       {formatPrice && (
-        <BiDollar
-          size={24}  
-          className="
-            text-neutral-700
-            absolute
-            top-5
-            left-2
-          "
-        />
+        <BiDollar size={24} className="text-text absolute top-5 left-2"/>
       )}
       <input
         id={id}
@@ -70,16 +62,8 @@ export const Input = ({
         placeholder=" "
         type={type}
         className={`
-          peer
-          w-full
-          p-2
-          pt-6 
-          font-light 
-          bg-secondary 
-          border-2
-          rounded-md
-          outline-none
-          transition
+          peer w-full p-2 pt-6 font-light 
+          bg-secondary border-2 rounded-md outline-none transition
           disabled:opacity-70
           disabled:cursor-not-allowed
           ${formatPrice ? 'pl-9' : 'pl-4'}
@@ -89,14 +73,7 @@ export const Input = ({
       />
       <label 
         className={`
-          absolute 
-          text-md
-          duration-150 
-          transform 
-          -translate-y-3 
-          top-4 
-          z-10 
-          origin-[0] 
+          absolute text-md duration-150 transform -translate-y-3 top-4 z-10 origin-[0] 
           ${formatPrice ? 'left-9' : 'left-4'}
           peer-placeholder-shown:scale-100 
           peer-placeholder-shown:translate-y-0 
