e8dcc83e30690a3a577dbaf276e09ba5616bd7bc
Gabriel Fu <hfu.gabriel@gmail.com>
2023-05-31 15:26:49 +0800

    add footer

--- frontend/app/components/modals/PortfolioModal.tsx
+++ frontend/app/components/modals/PortfolioModal.tsx
@@ -3,8 +3,10 @@
 import { useState } from "react";
 import { toast } from "react-hot-toast";
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+import { FcGoogle } from "react-icons/fc"
 import Modal, { Heading, Input } from "./Modal";
 import usePortfolioModal from "@/app/hooks/usePortfolioModal";
+import Button from "../Button";
 
 const PortfolioModal = () => {
   const portfolioModal = usePortfolioModal();
@@ -59,6 +61,37 @@ const PortfolioModal = () => {
     </div>
   );
 
+  const footerContent = (
+    <div className="flex flex-col gap-4 mt-3">
+      <hr />
+      <Button 
+        outline 
+        label="Continue with Google"
+        icon={FcGoogle}
+        onClick={() => {}} 
+      />
+      <div 
+        className="
+          text-neutral-500 
+          text-center 
+          mt-4 
+          font-light
+        "
+      >
+        <p>Already have an account?
+          <span 
+            // onClick={onToggle} 
+            className="
+              text-neutral-800
+              cursor-pointer 
+              hover:underline
+            "
+            > Log in</span>
+        </p>
+      </div>
+    </div>
+  )
+
   return ( 
     <Modal 
       disabled={isLoading}
@@ -68,6 +101,7 @@ const PortfolioModal = () => {
       title="Create New Portfolio"
       actionLabel="Create"
       body={bodyContent}
+      footer={footerContent}
     />
    );
 }
